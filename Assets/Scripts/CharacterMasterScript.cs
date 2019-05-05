6aacedecefc165b3d3a052b3acb56939959d8862
BlitzBurn <JK98@Live.se>
2019-05-05 22:27:27 +0200

    FirstCommit, just fookin about

---
+++ Assets/Scripts/CharacterMasterScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMasterScript : MonoBehaviour
+{
+    public MovementController movementController;
+    private bool combatMode;
+
+    void Start()
+    {
+        movementController = GetComponent<MovementController>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&combatMode==false)
+        {
+            movementController.enabled = false;
+            combatMode = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space)&&combatMode==true)
+        {
+            movementController.enabled = true;
+            combatMode = false;
+        }
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [Range(0,1)]
+    public float movementSpeed;
+
+    private Rigidbody playerRB;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + new Vector3(0,0,movementSpeed);            
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + new Vector3(0,0,-movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + new Vector3(-movementSpeed,0,0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3(movementSpeed,0, 0);
+        }
+    }
+}
